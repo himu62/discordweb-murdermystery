8bffc937595170c10c62410126d8e9b2b641034a
himu62 <zero.256dic@gmail.com>
2023-03-02 22:52:51 +0900

    helperText

--- src/atom/TextField.tsx
+++ src/atom/TextField.tsx
@@ -5,16 +5,26 @@ import { FC, useRef, useState } from "react";
 const TextField: FC<Omit<TextFieldProps, "error" | "inputRef" | "onChange">> = (
   props
 ) => {
-  const helperText = props.helperText ?? "";
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { helperText: _, ..._props } = props;
-
   const ref = useRef<HTMLInputElement>();
   const [error, setError] = useState(false);
+  const [helperText, setHelperText] = useState<typeof props.helperText>("");
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { helperText: _, ..._props } = props;
 
   const handleChange = () => {
     if (ref.current) {
       setError(!ref.current?.validity.valid);
+
+      if (ref.current?.validity.valid) {
+        setHelperText("");
+      } else {
+        if (props.required && !ref.current?.value) {
+          setHelperText("必須項目です");
+        } else {
+          setHelperText(props.helperText ?? "");
+        }
+      }
     }
   };
 
@@ -22,7 +32,7 @@ const TextField: FC<Omit<TextFieldProps, "error" | "inputRef" | "onChange">> = (
     <MuiTextField
       error={error}
       inputRef={ref}
-      helperText={error && helperText}
+      helperText={helperText}
       color={error ? "error" : "info"}
       onChange={handleChange}
       {..._props}
